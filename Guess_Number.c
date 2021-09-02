22ea8a4900550e7386f4f5dd7b98052815987beb
Nessvah <vanessamarquesns@outlook.pt>
2021-09-02 15:42:50 +0100

    Generate random number

--- Guess_Number.c
+++ Guess_Number.c
@@ -4,6 +4,17 @@
 
 int main(void)
 {
+    //Time variable
+    time_t t; 
+
+    //Initialize random number
+    srand ((unsigned) time (&t));
+
+    //Get random number and store it in a variable
+    int randomNumber = rand() % 21;
+
+    
+    
     printf("First commit \n");
     return 0;
 }
